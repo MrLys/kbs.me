de010fdd9586361f803516b01cd64db3fdcb5fe0
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2024-04-24 14:31:34 +0200

    add README with commands and initial keymap

---
+++ README.md
@@ -0,0 +1,4 @@
+#  QMK Firmware for Wilba Tech WT65-G (Actually it's a WT65-h4)
+## USAGE
+`cp keymap.c $HOME/qmk_firmware/keyboards/wilba_tech/wt65_g/keymaps/mrlys/`
+`qmk flash -kb wilba_tech/wt65_g`
---
+++ keymap.c
@@ -0,0 +1,67 @@
+#include QMK_KEYBOARD_H
+
+ #define GRAVE_MODS  (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
+
+/*
+*
+* ,-------------------------------------------------------------------------------------------------------------------.
+* | ESC  |  7   |  5   |  3   |  3   |  1   |  9   |  0   |  2   |  4   |  6   |  8   |  NO  | BSPC | DEL  |
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+* | TAB  |  ;   |  ,   |  .   |  P   |  Y   |  F   |  G   |  C   |  R   |  L   |  /   |  NO  | END  | ESC  |
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+* |  A   |  O   |  E   |  U   |  I   |  D   |  H   |  T   |  N   |  S   |  -   |  \   |  EN  |      | LSFT |
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+* | TO 1 |  '   |  Q   |  J   |  K   |  X   |  B   |  M   |  W   |  V   |  Z   | RSFT |  UP  |      |      |
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+* | LCTL | LGUI | LALT | SPC  |      |      | LEFT | DOWN | RGHT |      |      |      |      |      |      |
+* `-------------------------------------------------------------------------------------------------------------------'
+*/
+enum layer_names {
+    _DVORAK,
+    _NAVIGATION,
+    _SYMBOLS,
+    _QWERTY
+
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[_DVORAK] = LAYOUT_65_iso_blocker(
+        KC_ESC,     KC_7,           KC_5,       KC_3,       KC_3,       KC_1,       KC_9,       KC_0,       KC_2,       KC_4,       KC_6,       KC_8,       KC_NO,      KC_BSPC,        KC_DEL,
+        KC_TAB,     KC_SCLN,        KC_COMM,    KC_DOT,     KC_P,       KC_Y,       KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_SLSH,    KC_NO,      /*nothing*/     KC_END,
+        KC_ESC,     KC_A,           KC_O,       KC_E,       KC_U,       KC_I,       KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_MINS,    KC_BSLS,    KC_ENT,         KC_NO,
+        KC_LSFT,    TO(1),          KC_QUOT,    KC_Q,       KC_J,       KC_K,       KC_X,       KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,       KC_RSFT,    KC_UP,          KC_NO,
+        KC_LCTL,    LGUI(KC_NO),    KC_LALT,    /*----------------------------space-----------------------------*/      KC_SPC,     KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,        KC_RGHT
+    ),
+	[_NAVIGATION] = LAYOUT_65_iso_blocker(
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      /*nothin*/      KC_NO,
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      TO(0),        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      
+        KC_LCTL,    LGUI(KC_NO),    KC_LALT,    /*----------------------------space-----------------------------*/      KC_SPC,     KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,        KC_RGHT
+    ),
+	[_SYMBOLS] = LAYOUT_65_iso_blocker(
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      /*nothin*/      KC_NO,
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      TO(0),        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      
+        KC_LCTL,    LGUI(KC_NO),    KC_LALT,    /*----------------------------space-----------------------------*/      KC_SPC,     KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,        KC_RGHT
+    ),
+	[_QWERTY] = LAYOUT_65_iso_blocker(
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      /*nothin*/      KC_NO,
+        KC_ESC,     KC_NO,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,
+        KC_NO,      TO(0),        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      
+        KC_LCTL,    LGUI(KC_NO),    KC_LALT,    /*----------------------------space-----------------------------*/      KC_SPC,     KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,        KC_RGHT
+    )
+};
+
+#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+
+};
+#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+
+
+
+
